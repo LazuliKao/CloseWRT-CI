25f116a7a667627503bece483d4ea29b90049e33
LazuliKao <LazuliKao233@outlook.com>
2024-08-11 10:33:09 +0800

    update

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -20,7 +20,10 @@ UPDATE_PACKAGE() {
 	fi
 }
 
-UPDATE_PACKAGE "luci-app-dockerman" "kenzok8/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "luci-app-wan-mac" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-cloudflarespeedtest" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "dockerd" "kenzok8/small-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-dockerman" "kenzok8/small-package" "main" "pkg"
 # UPDATE_PACKAGE "luci-app-ddns-go" "kenzok8/openwrt-packages" "master" "pkg"
 # UPDATE_PACKAGE "luci-app-store" "kenzok8/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "luci-app-alist" "kenzok8/openwrt-packages" "master" "pkg"
@@ -34,9 +37,9 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "js"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
+# UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
-UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+# UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
